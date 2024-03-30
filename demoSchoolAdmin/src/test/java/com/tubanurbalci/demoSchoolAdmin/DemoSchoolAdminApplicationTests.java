ff35360065413f2316f8c35686044c00262a5969
Tubanur Balci <tubanurbalci5015@gmail.com>
2024-03-30 11:58:16 +0300

    Spring Boot Admin Project used for DemoSchool Project

---
+++ demoSchoolAdmin/src/test/java/com/tubanurbalci/demoSchoolAdmin/DemoSchoolAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tubanurbalci.demoSchoolAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSchoolAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
